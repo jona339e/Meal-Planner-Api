83633f6b4ef9fd71e4adfe870d195b1140a5f261
Jonas Østergaard <Jonasoe@hotmail.com>
2023-08-23 13:58:37 +0200

    Add project files.

---
+++ Meal-Planner-Api/Controllers/RecipeController.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using Meal_Planner_Api.Dto;
+using Meal_Planner_Api.Interfaces;
+using Meal_Planner_Api.Models;
+using Meal_Planner_Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Meal_Planner_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRecipeRepository _recipeRepository;
+        private readonly IAmountRepository _amountRepository;
+        private readonly IMapper _mapper;
+
+        public RecipeController(IRecipeRepository recipeRepository, IAmountRepository amountRepository, IMapper mapper)
+        {
+            _recipeRepository = recipeRepository;
+            _amountRepository = amountRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllRecipes()
+        {
+            List<RecipeDTO> recipes = _recipeRepository.GetAllRecipes()
+                .Select(recipe => _mapper.Map<RecipeDTO>(recipe))
+                .ToList();
+            return Ok(recipes);
+        }
+
+        [HttpGet("{id}", Name = "GetRecipe")]
+        public ActionResult<RecipeDTO> GetRecipe(int id)
+        {
+            var recipe = _recipeRepository.GetRecipe(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            var recipeDTO = _mapper.Map<RecipeDTO>(recipe);
+            return Ok(recipeDTO);
+        }
+
+        [HttpPost]
+        public ActionResult<RecipeDTO> CreateRecipe([FromBody] Recipe recipe)
+        {
+            _recipeRepository.AddRecipe(recipe);
+
+
+            return CreatedAtAction(nameof(GetRecipe), new { id = recipe.Id }, recipe);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateRecipe(int id, RecipeDTO recipeUpdateDto)
+        {
+            var recipeModel = _mapper.Map<Recipe>(recipeUpdateDto);
+            recipeModel.Id = id; // Make sure to set the ID to the existing recipe's ID
+            _recipeRepository.UpdateRecipe(recipeModel);
+
+            return NoContent(); // Returns a 204 No Content response
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteRecipe(int id)
+        {
+            var recipe = _recipeRepository.GetRecipe(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _recipeRepository.DeleteRecipe(id);
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Meal-Planner-Api/Repositories/RecipeRepository.cs
@@ -0,0 +1,60 @@
+﻿using Meal_Planner_Api.Data;
+using Meal_Planner_Api.Dto;
+using Meal_Planner_Api.Interfaces;
+using Meal_Planner_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Meal_Planner_Api.Repositories
+{
+    public class RecipeRepository : IRecipeRepository
+    {
+        private readonly DataContext _context;
+
+        public RecipeRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Recipe GetRecipe(int id)
+        {
+            return _context.Recipes
+                            .Include(r => r.Ingredients)
+                                .ThenInclude(i => i.Amounts)
+                            .Include(r => r.Instructions)
+                            .SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Recipe> GetAllRecipes()
+        {
+            return _context.Recipes
+                .Include(r => r.Ingredients)
+                    .ThenInclude(i => i.Amounts) // Include Amounts within Ingredients
+                .Include(r => r.Instructions)
+                .ToList();
+        }
+
+        public Recipe AddRecipe(Recipe recipe) { 
+            _context.Recipes.Add(recipe);
+            _context.SaveChanges();
+            return recipe;
+        }
+
+        public void UpdateRecipe(Recipe recipe)
+        {
+            _context.Entry(recipe).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void DeleteRecipe(int id)
+        {
+            var recipe = _context.Recipes.FirstOrDefault(r => r.Id == id);
+            if (recipe != null)
+            {
+                _context.Recipes.Remove(recipe);
+                _context.SaveChanges();
+            }
+        }
+
+
+    }
+}
